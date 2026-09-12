cd2e7a29fb76bc558306df4768d04a87211545e1
akanni <alhassanfijabi@proton.me>
2026-09-12 10:43:37 +0100

    cleaned up code logic

--- 4_2.cpp
+++ 4_2.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
+#define GRAVITY 9.8
+double model_drop(float height,int second) {
+    double covered {0.5*GRAVITY*second*second};
+    if (covered > 0) {
+        return covered;
+    }
+    else {
+        return 0;
+    }
+
+};
+
 
 /*Tower sim model*/
 int main() {
-    float gravity{9.81};
+
 
     std::cout << "Enter the height of tower: ";
-    float height;
+    float height{};
     std::cin >> height;
 
-    std::cout<< "At 0 seconds, the ball is at height: "<< height << '\n';
+    std::cout<< "At 0 seconds, the ball is at height: "<< height- model_drop(100,0)<< '\n';
+    std::cout<< "At 1 seconds, the ball is at height: "<< height- model_drop(100,1) << '\n';
+    std:: cout<< "At 2 seconds, the ball is at height: "<< height - model_drop(100,2)<< '\n';
+    std::cout << "At 3 seconds, the ball is at height: "<< height- model_drop(100,3) << '\n';
+    std::cout<< "At 4 seconds, the ball is at height: "<< height -model_drop(100,4)<< '\n';
+    std::cout<< "At 5 seconds, the ball is at height: "<< height -model_drop(100,5)<< '\n';
+
+
 
 
 
