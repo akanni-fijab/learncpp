bfabc495e26862ea7376f07a9a2c4b06f52c67f3
akanni <alhassanfijabi@proton.me>
2026-09-12 10:06:20 +0100

    added function logic

--- 4_1.cpp
+++ 4_1.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <cstdint>
+double operation(double first_no, double second_no,char opr) {
 
+    if (opr == '+') {
+        return first_no + second_no;
+    }
+    else if (opr == '-') {
+        return first_no - second_no;
+    }
+    else if (opr =='*') {
+        return  first_no * second_no;
+    }
+    else if (opr =='/')
+        return first_no / second_no;
+    else
+        return 1.0; // gotta return an int
+};
 
 
 int main() {
     std::cout << "Enter a double value: ";
-
     double first_no{};
     std::cin >> first_no;
 
@@ -12,8 +27,12 @@ int main() {
     double second_no{};
     std::cin >> second_no;
 
-    std::cout << first_no + second_no;
 
+    std::cout << "Operation * / + - ";
+    char opr{};
+    std::cin >> opr;
+
+    std::cout << "The result of "<<first_no <<opr<<second_no<<" is "<<operation(first_no, second_no, opr)<<std::endl;
 
     return  0;
 }
